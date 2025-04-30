c79777feb5b2d0b2cf21b7f4fae97a5ca4115cc2
AnonymousRand <anonymouserand@gmail.com>
2025-04-30 04:37:58 +0000

    add debug functions for testing dead links on pages in browser console

--- app/static/js/util.js
+++ app/static/js/util.js
@@ -2,19 +2,46 @@ const HORIZ_SCOLL_DIV_HTML = '<div class="scroll-overflow-x"></div>';
 const HORIZ_SCOLL_DIV_HTML_FULL_WIDTH = '<div class="scroll-overflow-x" width="full"></div>';
 
 /**
+ * Adds code to an existing function/merges two functions.
+ *
  * Preconditions:
- *     - Target function(s) must have already been initialized
+ *     - The two functions must have the same params
  *
  * Usage:
  *     ```
  *     func1 = addToFunction(func1, func2);
  *     ```
- *
- *     Make sure the two funcs have the same params.
  */
 function addToFunction(funcBase, funcToAdd) {
     return function() {
         funcBase.apply(this, arguments);
         funcToAdd.apply(this, arguments);
     };
+}
+
+await function debugTestExternalLinks() {
+    $("a").each(function() {
+        let dest = $(this).attr("href");
+        if (dest && !dest.startsWith("#")) {
+            const resp = await fetch(dest, {method: "GET"})
+            if (resp.status.toString().startsWith("4")) {
+                console.log(`${dest} returns ${resp.status}`);
+            }
+        }
+    });
+}
+
+/**
+ * Debug use: prints out dead self-links on current page.
+ */
+function debugTestSelfLinks() {
+    $("a").each(function() {
+        let dest = $(this).attr("href");
+        // don't test footnotes since they have special syntax and will always be marked as dead
+        if (dest && dest.startsWith("#") && !dest.startsWith("#fn")) {
+            if ($(dest).length === 0) {
+                 console.log(dest);
+            }
+        }
+    });
 }
