0ff1f528f3f9d1d9e2568a4adf5561057f5e6671
AnonymousRand <yhugdrive@gmail.com>
2025-05-02 04:57:24 +0000

    rawr >:3

--- app/static/js/events.js
+++ app/static/js/events.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $("#meow-btn").on("click", function() {
-        let meow = Math.floor(Math.random() * 10) < 1 ? "rawr :333333" : "meow :3";
+        let meow = Math.floor(Math.random() * 10) < 1 ? "rawr >:3" : "meow :3";
         flashMessage(meow);
     });
 });
