6d8d2db5c9b9717ed1b7c565db0322b5bd434ed4
AnonymousRand <yhugdrive@gmail.com>
2025-10-07 17:17:23 +0000

    working?

--- app/blog/static/blogpage/js/post_collapsible_sections.js
+++ app/blog/static/blogpage/js/post_collapsible_sections.js
@@ -1,7 +1,7 @@
 const postScrollObserver = new IntersectionObserver(function(entries) {
     entries.forEach(function(entry) {
         if (entry.isIntersecting) {
-            entry.target.setAttribute("open", "");
+            entry.target.closest(".post__collapsible-section").setAttribute("open", "");
         }
     });
 });
@@ -9,15 +9,19 @@ const postScrollObserver = new IntersectionObserver(function(entries) {
 $("#post__content h2").each(function() {
     $(this).nextUntil("#post__content h2, #footnotes__wrapper, #post__end").addBack().wrapAll('<details class="post__collapsible-section"></details>');
     $(this).wrap('<summary></summary>');
-    // automatically expand section when on screen
-    postScrollObserver.observe($(this).closest("details.post__collapsible-section").get(0));
 });
 
-$(document).ready(function() {
-    $(".post__collapsible-section").on("toggle", function(e) {
-        if (e.target.open) {
-            renderMathJaxNode(e.target);
-            applyStylesNode(e.target);
-        }
+$(".post__collapsible-section").on("toggle", function(e) {
+    if (e.target.open) {
+        renderMathJaxNode(e.target);
+        applyStylesNode(e.target);
+    }
+});
+
+// automatically expand collapsible sections when on screen
+$(".post__collapsible-section").each(function() {
+    postScrollObserver.observe($(this).get(0));
+    $(this).find("p").each(function() {
+        postScrollObserver.observe($(this).get(0));
     });
 });
--- app/blog/static/blogpage/js/post_events.js
+++ app/blog/static/blogpage/js/post_events.js
@@ -1,3 +1,17 @@
+function onUrlFragmentNavigate(urlFragment) {
+    const jqTarget = $(urlFragment);
+    if (jqTarget.length === 0) {
+        return;
+    }
+
+    const jqParentCollapsible = jqTarget.parents("details");
+    if (jqParentCollapsible.length > 0) {
+        jqParentCollapsible.attr("open", "");
+    }
+    // may need to manually correct scrolling position
+    jqTarget.get(0).scrollIntoView();
+}
+
 $(document).ready(function() {
     $("#copy-permanent-link-btn").on("click", function() {
         navigator.clipboard.writeText(PERMANENT_LINK_URL);
@@ -15,4 +29,14 @@ $(document).ready(function() {
         navigator.clipboard.writeText(url);
         flashMsg(`Link copied: ${url}`);
     });
+
+    // open necessary collapsible if navigated to URL fragment on page load
+    if (document.location.hash !== "") {
+        onUrlFragmentNavigate(document.location.hash);
+    }
+});
+
+// open necessary collapsible if navigated to URL fragment
+$(window).on("hashchange", function() {
+    onUrlFragmentNavigate(document.location.hash);
 });
